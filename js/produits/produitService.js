c80771899ca2699cf0306edcce4a5d9ce2e49cc9
maxoumime <maxime.bertheau@hotmail.fr>
2015-04-06 15:32:16 +0200

    Toastr de connexion + externalisation de l'url hôte

--- js/produits/produitService.js
+++ js/produits/produitService.js
@@ -4,16 +4,16 @@
 produitModule.factory('produitService', ['$http', '$rootScope', function ($http, $rootScope){
     var factory = {};
     factory.getlist = function(){
-        return $http.get('http://localhost:8080/rest/produit?token='
+        return $http.get(host+'/rest/produit?token='
             + $rootScope.token
         ).error(function(data){
                 delete $rootScope.token;
-            });
+        });
     };
     factory.commander = function(produit)
     {
         return $http.post(
-            'http://localhost:8080/rest/Commande',
+            host+'/rest/Commande',
             {token: $rootScope.token, reference: produit.reference, quantityOrdered: produit.quantityOrdered}
         )
     };
